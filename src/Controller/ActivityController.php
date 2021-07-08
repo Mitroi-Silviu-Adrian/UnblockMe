130b5aea08008ad30facde04360587231b35aa71
Mitroi-Silviu-Adrian <73160992+Mitroi-Silviu-Adrian@users.noreply.github.com>
2021-07-08 20:40:49 +0300

    Report, Delete Activity and Show Activities

--- src/Controller/ActivityController.php
+++ src/Controller/ActivityController.php
@@ -4,6 +4,8 @@ namespace App\Controller;
 
 use App\Entity\LicensePlates;
 use App\Repository\ActivityRepository;
+use App\Form\ActivityType;
+use phpDocumentor\Reflection\Types\This;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -15,32 +17,60 @@ use App\Entity\User;
 class ActivityController extends AbstractController
 {
     #[Route('/activity', name: 'activity')]
-    public function index(): Response
+    public function index(ActivityRepository $activityRepository): Response
     {
+        $licensePlates = $this->getUser()->getLicensePlates();
+
+        $activities = array();
+
+        foreach($licensePlates as $licensePlate)
+        {
+            $planeLP = $licensePlate->getLicensePlate();
+
+            $result = $activityRepository->findByBlocker($planeLP);
+
+            if($result)
+                array_push($activities,$result);
+
+            $result = $activityRepository->findByBlockee($planeLP);
+
+            if($result)
+                array_push($activities,$result);
+
+        }
+
         return $this->render('activity/index.html.twig', [
             'controller_name' => 'ActivityController',
+            'activityRepository' => $activityRepository->findAll(),
+            'activeActivities' => $activities,
         ]);
     }
 
     #[Route('/activity/{type}', name: 'activityType', methods: ['GET', 'POST'])]
-    public function activityType(Request $request, string $type): Response
+    public function activityType(Request $request,
+                                 string $type,
+                                 ActivityRepository $activityRepository): Response
     {
         return $this->render('activity/chooseCar.html.twig', [
             'license_plates' => $this->getUser()->getLicensePlates(),
             'type' => $type,
+            'activityRepository' => $activityRepository,
         ]);
     }
 
     #[Route('/activity/choose/{type}/{id}', name: 'chooseLP', methods: ['GET', 'POST'])]
-    public function choose(Request $request, string $type, LicensePlates $licensePlates): Response
+    public function choose(Request $request,
+                           string $type,
+                           LicensePlates $licensePlates,
+                           ActivityRepository $activityRepository): Response
     {
         $licensePlatesPlain = $licensePlates->getLicensePlate();
 
-        $activities = $this->getDoctrine()->getRepository(Activity::class)->findAll();
+        //$activities = $this->getDoctrine()->getRepository(Activity::class)->findAll();
 
         if($type == 'blocker')
         {
-            $result = $activities->findByBlocker();
+            $result = $activityRepository->findByBlocker($licensePlatesPlain);
 
             if($result != null)
             {
@@ -49,12 +79,12 @@ class ActivityController extends AbstractController
             }
 
             return $this->redirectToRoute('reportActivity', [
-                'licensePlates' => $licensePlates,
+                'id' => $licensePlates->getId(),
                 'type' => $type,
             ]);
         }
 
-        $result = $activities->findByBlockee();
+        $result = $activityRepository->findByBlockee($licensePlatesPlain);
 
         if($result != null)
         {
@@ -63,21 +93,55 @@ class ActivityController extends AbstractController
         }
 
         return $this->redirectToRoute('reportActivity', [
-            'licensePlates' => $licensePlates,
+            'id' => $licensePlates->getId(),
             'type' => $type,
         ]);
 
     }
 
-    #[Route('/reportActivity', name: 'reportActivity', methods: ['GET', 'POST'])]
+    #[Route('/reportActivity/{type}/{id}', name: 'reportActivity', methods: ['GET', 'POST'])]
     public function new(Request $request, string $type, LicensePlates $licensePlates): Response
     {
 
+        $activity = new Activity();
 
+        if($type == 'blocker')
+            $activity->setBlocker($licensePlates->getLicensePlate());
+        else
+            $activity->setBlockee($licensePlates->getLicensePlate());
+
+        $form = $this->createForm(ActivityType::class,$activity);
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid()) {
+            $activity = $form->getData();
+
+            //echo $activity->getBlocker();
+            //echo $activity->getBlockee();
+
+            $entityManager = $this->getDoctrine()->getManager();
+
+            $activity->setStatus(1);
+
+            $entityManager->persist($activity);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('activity');
+        }
+
+        //echo $licensePlates->getLicensePlate();
         return $this->render('activity/new.html.twig', [
-            //'licensePlate' => $licensePlate,
-            'type' => $type,
+            'activity' => $activity,
+            'form' => $form->createView(),
         ]);
     }
+    #[Route('/deleteActivity/{status}', name: 'deleteActivity', methods: ['GET', 'POST'])]
+    public function delete(Request $request, Activity $activity): Response
+    {
+        $entityManager = $this->getDoctrine()->getManager();
+        $entityManager->remove($activity);
+        $entityManager->flush();
 
+        return $this->redirectToRoute('activity');
+    }
 }
